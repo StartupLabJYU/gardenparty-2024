3f043a6b2b5e60db653f194cf45c13c2b4cf599b
Teemu Autto <teemu.a.autto@jyu.fi>
2024-09-11 11:40:09 +0000

    White balance function.

--- src/gardenparty/preprocess.py
+++ src/gardenparty/preprocess.py
@@ -171,9 +171,20 @@ def scale_and_crop(image, max_size=1024, aspect_ratio_max=2.5):
     return scaled_image
 
 
+def white_balance(img):
+    result = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    avg_a = np.average(result[:, :, 1])
+    avg_b = np.average(result[:, :, 2])
+    result[:, :, 1] = result[:, :, 1] - ((avg_a - 128) * (result[:, :, 0] / 255.0) * 1.1)
+    result[:, :, 2] = result[:, :, 2] - ((avg_b - 128) * (result[:, :, 0] / 255.0) * 1.1)
+    result = cv2.cvtColor(result, cv2.COLOR_LAB2BGR)
+    return result
+
+
 def autocrop(image_path, output_path) -> None:
     image, blurred = preprocess_image(image_path)
 
+    image = white_balance(image)
 
     image = trim_whitespace(image)
     # contrasted = enhance_contrast(cropped)
@@ -191,6 +202,9 @@ def autocrop(image_path, output_path) -> None:
 def autocrop_and_straighten(image_path, output_path) -> None:
     image, blurred = preprocess_image(image_path)
     #edged = detect_edges(blurred)
+    
+    image = white_balance(image)
+
     contour = find_document_contours(blurred)
     image = get_document_perspective(image, contour)
     
